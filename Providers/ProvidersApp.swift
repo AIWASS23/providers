21ea53c20158de243d2d3c28fe0d8c28afee950a
AIWASS23 <marcelodearaujo677@gmail.com>
2024-12-04 08:13:57 -0300

    Initial Commit

---
+++ Providers/ProvidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProvidersApp.swift
+//  Providers
+//
+//  Created by marcelodearaujo on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProvidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
